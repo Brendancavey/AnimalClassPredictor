dd0d279530968bb8c3b41d5f0bc82b658bd34bf2
Brendan <bthoeung@gmail.com>
2022-11-23 15:02:06 -0800

    Added dropdown menu for num of legs
    Added select dropdown menu for user interface to select number of legs. Changed bool value for "has legs" to the select dropdown menu value.

--- animalPredictor.py
+++ animalPredictor.py
@@ -12,22 +12,25 @@ def index():
     flash("What animal do you see yourself as?...")
     return render_template("index.html")
 
-@app.route('/prediction', methods=["POST", "GET"])
+@app.route('/prediction', methods=["POST"])
 def predict():
     #flash("A " + str(request.form['name_input']) + "? That sounds amazing!")
     list = request.form.getlist("animalfeatures")
-    animal_features_list = ["0", "1", "2", "3"]
+    animal_features_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
     pred_bool_list = []
 
     for i in range(0, len(animal_features_list)):
-        if animal_features_list[i] in list:
+        if animal_features_list[i] == "12": #using dropdown menu for number of legs value. 12 corresponds to num of legs
+            pred_bool_list.append(request.form.get("legs"))
+        elif animal_features_list[i] in list:
             pred_bool_list.append(1)
         else:
             pred_bool_list.append(0)
     print(pred_bool_list)
     print(list)
-    #prediction = model.predict(request.form.getlist("animalfeatures"))
-    #flash(prediction[0])
+    prediction = model.predict([pred_bool_list])
+    flash(prediction[0])
+    print(request.form.get("legs"))
     return render_template("index.html")
 
 
