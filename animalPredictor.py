b7bf67d112bf211cdb679cb0ed3b97738889a1b6
Brendan <bthoeung@gmail.com>
2022-12-16 13:37:09 -0800

    Transformed GUI
    Changed the user interface selection from checkboxes to radio buttons to better represent the questions and the data. Removed unnecessary code.

--- animalPredictor.py
+++ animalPredictor.py
@@ -25,22 +25,16 @@ def index():
 
 @app.route('/prediction', methods=["POST"])
 def predict():
-    list = request.form.getlist("animalfeatures") #gets the values from the checkboxes user selected
-    animal_features_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]
-
-    # lists for machine learning model
-    pred_bool_list = []  # empty list that will be used to make the prediction. Model only recognizes 1s and 0s as true false values in the order of the animal features list.
-
-    for i in range(0, len(animal_features_list)):
-        if animal_features_list[i] == "12": #using dropdown menu for number of legs value. 12 corresponds to num of legs in features list
-            pred_bool_list.append(request.form.get("legs"))
-        elif animal_features_list[i] in list:
-            pred_bool_list.append(1) #appending 1 if checkbox was checked indiciating true
+    #initializing user answers
+    user_answers = ["0"] * 16
+    #getting data from html form and placing into user answers list
+    for i in range(0, 16):
+        if i == 12: #using dropdown menu for number of legs value. 12 corresponds to num of legs in features list
+            user_answers[i] = request.form.get("legs")
         else:
-            pred_bool_list.append(0) #appending 0 if checkbox was not checked indicating false
-    print(pred_bool_list)
-    print(list)
-    prediction = model.predict([pred_bool_list]) #using imported model from Jupyter Notebooks to make prediction
+            user_answers[i] = request.form[str(i)]
+
+    prediction = model.predict([user_answers]) #using imported model from Jupyter Notebooks to make prediction
     flash("You are most likely a(n) " + prediction[0] + "!") #prediction is the 0th index in the list
     hidden_msg = "Animals to check out!"
     #Checking for condition to link predicted output to more information via wiki
@@ -65,15 +59,15 @@ def predict():
     if (prediction[0] == "Invertebrate"):
         animal_list = invertebrates
         webpage = "https://en.wikipedia.org/wiki/Invertebrate"
-    print(request.form.get("legs"))
 
     # checking for condition verify which checkboxes were selected to output
     # most meaningful graph charts
-    bool_list = [False] * len(pred_bool_list)  # true/false value list to compare to prediction_bool_list
-    for i in range(0, len(pred_bool_list)):
-        if (pred_bool_list[i] == 1):
+    bool_list = [False] * len(user_answers)  # true/false value list to compare to prediction_bool_list
+    for i in range(0, len(user_answers)):
+        if (user_answers[i] == "1"):
             bool_list[i] = True
     print(bool_list)
+
     return render_template("index.html", predicted_list = animal_list, link = webpage, reveal_msg = hidden_msg, predicted_animal = prediction[0], data_button = True, reveal_traits = True,
                            hair = bool_list[0],
                            feathers = bool_list[1],
@@ -87,7 +81,7 @@ def predict():
                            breathes = bool_list[9],
                            venomous = bool_list[10],
                            fins = bool_list[11],
-                           legs = pred_bool_list[12],
+                           legs = user_answers[12],
                            tail = bool_list[13],
                            domestic = bool_list[14],
                            catsize = bool_list[15])
